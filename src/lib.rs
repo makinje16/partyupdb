0bb8f46072fadd4d8ae4e8c3f9f3d02332e64450
Malcolm Akinje <malcolm.akinje@gmail.com>
2019-01-02 21:37:16 -0500

    Added pub functions to insert delte and query players. Needs testing***

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,27 @@ pub fn create_player<'a>(conn: &PgConnection, username: &'a str, discord_name: &
         .get_result(conn)
         .expect("Error saving new Player")
 }
+
+pub fn insert_player(username: &str, discord_name: &str, rank: &Rank) {
+    let conn = establish_connection();
+    create_player(&conn, username, discord_name, rank);
+}
+
+pub fn delete_player(discord_name: &str) {
+    use self::schema::players::dsl::*;
+    let conn = establish_connection();
+    match diesel::delete(players.filter(username.eq(discord_name))).execute(&conn) {
+        Ok(_ok) => (),
+        Err(why) => println!("Error: {}", why),
+    }
+}
+
+pub fn look_for_by_rank(player_rank: Rank) -> std::vec::Vec<models::Player> {
+    use self::schema::players::dsl::*;
+
+    let conn = establish_connection();
+    players.filter(rank.eq(player_rank.to_int()))
+        .limit(10)
+        .load::<Player>(&conn)
+        .expect("Error loading players")
+}
