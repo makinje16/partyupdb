3a5c5d2c974cd13241bb0502b5c024e3806990ad
Malcolm Akinje <malcolm.akinje@gmail.com>
2019-01-11 23:57:30 -0500

    delete_players now takes no arguments and deletes all players in the db

--- src/bin/delete_players.rs
+++ src/bin/delete_players.rs
@@ -3,13 +3,10 @@ extern crate lfgdb;
 
 use self::diesel::prelude::*;
 use self::lfgdb::*;
-use std::env::args;
 
 fn main() {
-    use lfgdb::models::Rank;
     use lfgdb::schema::players::dsl::*;
     let connection = establish_connection();
-    let name = args().nth(1).expect("Expected a target to match against");
     let _num_deleted =
-        diesel::delete(players.filter(rank.eq(Rank::Gold.to_int()))).execute(&connection);
+        diesel::delete(players.filter(discord_name.eq(discord_name))).execute(&connection);
 }
